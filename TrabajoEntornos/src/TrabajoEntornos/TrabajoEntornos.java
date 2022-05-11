dbb3bf9de33d09ed17a45c9b2cb57ea082945562
adrmon <vdrivn97@gmail.com>
2022-05-11 13:44:53 +0200

    Añadido la de g

--- TrabajoEntornos/src/TrabajoEntornos/TrabajoEntornos.java
+++ TrabajoEntornos/src/TrabajoEntornos/TrabajoEntornos.java
@@ -43,14 +43,16 @@ import java.awt.event.ActionEvent;
 
 public class TrabajoEntornos extends JPanel {
 
-	private static Alimento patatasFritas = new Alimento("Patatas Fritas", 7.47, 10.96, 49.74, 4.12, 6.56, 525, 547);
+	private static Alimento patatasFritas = new Alimento("Patatas Fritas", 7.47, 10.96, 49.74, 4.12, 6.56, 0.76, 273);
 	private static Alimento alitasDePollo = new Alimento("Alitas de Pollo", 19.3, 5.4, 0, 0, 26.64, 404, 288);
 	private static Alimento heladoVainilla = new Alimento("Helado de Vainilla", 11, 6.79, 23.6, 21.22, 33.5, 80, 201);
 	private static Alimento gominolas = new Alimento("Gominolas", 0.05, 0, 93.55, 70, 0, 50, 375);
 	private static Alimento brocoli = new Alimento("Brocoli", 0.37, 0.039, 6.64, 1.7, 2.82, 33, 34);
 	private static Alimento coliflor = new Alimento("Coliflor", 0.1, 0.032, 5.3, 2.4, 1.98, 30, 25);
+	private static Alimento chocolate = new Alimento("chocolate", 0.05, 0, 93.55, 70, 0, 50, 375);
+	private static Alimento merluza = new Alimento("Brocoli", 0.37, 0.039, 6.64, 1.7, 2.82, 33, 34);
+	private static Alimento piza = new Alimento("Coliflor", 0.1, 0.032, 5.3, 2.4, 1.98, 30, 25);
 	private static ArrayList<Alimento> listaAlimentos;
-	
 
 	private JFrame frmValoracion;
 	private JTable table;
@@ -66,22 +68,23 @@ public class TrabajoEntornos extends JPanel {
 	private JTextField textField_8;
 	private JTextField textField_16;
 	private JTextField textField_17;
-	private JTextField textField_25;
-	private JTextField textField_26;
 	private JTextField textField_9;
-	private JTextField textField_10;
 	private JTextField textField_11;
-	private JTextField textField_12;
 	private JTextField textField_15;
-	private JTextField textField_18;
 	private JTextField textField_19;
-	private JTextField textField_20;
 	private JTextField textField_21;
-	private JTextField textField_22;
 	private JTextField textField_23;
-	private JTextField textField_24;
 	private JTextField textField_13;
-	private JTextField textField_14;
+	private JLabel lblGr;
+	private JLabel lblGr_1;
+	private JLabel lblGr_2;
+	private JLabel lblGr_3;
+	private JLabel lblGr_4;
+	private JLabel lblGr_5;
+	private JLabel lblGr_6;
+	private JLabel lblGr_7;
+	private JLabel lblGr_8;
+	private JLabel lblGr_9;
 
 	public TrabajoEntornos() {
 
@@ -95,7 +98,6 @@ public class TrabajoEntornos extends JPanel {
 	public static void main(String[] args) {
 		// ARRAY LIST CREADO
 		listaAlimentos = new ArrayList<Alimento>();
-		
 
 		listaAlimentos.add(patatasFritas);
 		listaAlimentos.add(alitasDePollo);
@@ -128,7 +130,7 @@ public class TrabajoEntornos extends JPanel {
 		frmValoracion = new JFrame();
 		frmValoracion.setTitle("Valoracion de la Dieta");
 
-		frmValoracion.setBounds(100, 100, 974, 677);
+		frmValoracion.setBounds(100, 100, 980, 750);
 		frmValoracion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		Scanner tec = new Scanner(System.in);
@@ -138,12 +140,12 @@ public class TrabajoEntornos extends JPanel {
 		table.setBorder(new LineBorder(new Color(255, 200, 0), 3, true));
 		table.setRowHeight(33);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		table.setBounds(53, 41, 763, 330);
+		table.setBounds(34, 41, 795, 330);
 
 		Object[][] data;
 
 		table.setModel(new DefaultTableModel(new Object[][] {
-				{ "Nombre", "Grasas", "G Saturadas", "Hidratos", "Azucares", "Proteinas", "Sal", "Calorias" },
+				{ "Nombre", "Grasas (g)", "G Saturadas (g)", "Hidratos (g)", "Azucares (g)", "Proteinas (g)", "Sal (g)", "Calorias (g)" },
 				{ String.valueOf(patatasFritas.getNombre()), String.valueOf(patatasFritas.getGrasas()),
 						String.valueOf(patatasFritas.getGrasas_saturadas()),
 						String.valueOf(patatasFritas.getHidratos()), String.valueOf(patatasFritas.getAzucar()),
@@ -198,7 +200,7 @@ public class TrabajoEntornos extends JPanel {
 		JButton btnNewButton = new JButton("Valorar Dieta");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 			}
 		});
 		btnNewButton.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
@@ -211,10 +213,10 @@ public class TrabajoEntornos extends JPanel {
 		textArea.setEditable(false);
 		textArea.setBounds(509, 500, 371, 99);
 		frmValoracion.getContentPane().add(textArea);
-		
+
 		textField = new JTextField();
 		textField.setText("0");
-	 
+
 		textField.setBorder(new LineBorder(Color.GRAY, 3, true));
 		textField.setBounds(841, 78, 76, 20);
 		frmValoracion.getContentPane().add(textField);
@@ -275,129 +277,122 @@ public class TrabajoEntornos extends JPanel {
 		textField_8.setBorder(new LineBorder(Color.GRAY, 3, true));
 		textField_8.setBounds(841, 338, 76, 20);
 		frmValoracion.getContentPane().add(textField_8);
-		
+
 		textField_16 = new JTextField();
 		textField_16.setColumns(10);
 		textField_16.setBounds(53, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_16);
-		
+
 		textField_17 = new JTextField();
 		textField_17.setText("0");
 		textField_17.setColumns(10);
 		textField_17.setBorder(new LineBorder(Color.GRAY, 3, true));
 		textField_17.setBounds(841, 411, 76, 20);
 		frmValoracion.getContentPane().add(textField_17);
-		
-		textField_25 = new JTextField();
-		textField_25.setColumns(10);
-		textField_25.setBounds(53, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_25);
-		
-		textField_26 = new JTextField();
-		textField_26.setText("0");
-		textField_26.setColumns(10);
-		textField_26.setBorder(new LineBorder(Color.GRAY, 3, true));
-		textField_26.setBounds(841, 443, 76, 20);
-		frmValoracion.getContentPane().add(textField_26);
-		
+
 		JLabel lblAadeTusPropios = new JLabel("Añade tus propios alimentos y escribe la cantidad en gramos.");
 		lblAadeTusPropios.setBounds(195, 378, 455, 17);
 		frmValoracion.getContentPane().add(lblAadeTusPropios);
-		
+
 		textField_9 = new JTextField();
 		textField_9.setColumns(10);
 		textField_9.setBounds(148, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_9);
-		
-		textField_10 = new JTextField();
-		textField_10.setColumns(10);
-		textField_10.setBounds(148, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_10);
-		
+
 		textField_11 = new JTextField();
 		textField_11.setColumns(10);
 		textField_11.setBounds(242, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_11);
-		
-		textField_12 = new JTextField();
-		textField_12.setColumns(10);
-		textField_12.setBounds(242, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_12);
-		
+
 		textField_15 = new JTextField();
 		textField_15.setColumns(10);
 		textField_15.setBounds(434, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_15);
-		
-		textField_18 = new JTextField();
-		textField_18.setColumns(10);
-		textField_18.setBounds(434, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_18);
-		
+
 		textField_19 = new JTextField();
 		textField_19.setColumns(10);
 		textField_19.setBounds(530, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_19);
-		
-		textField_20 = new JTextField();
-		textField_20.setColumns(10);
-		textField_20.setBounds(530, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_20);
-		
+
 		textField_21 = new JTextField();
 		textField_21.setColumns(10);
 		textField_21.setBounds(626, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_21);
-		
-		textField_22 = new JTextField();
-		textField_22.setColumns(10);
-		textField_22.setBounds(626, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_22);
-		
+
 		textField_23 = new JTextField();
 		textField_23.setColumns(10);
 		textField_23.setBounds(721, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_23);
-		
-		textField_24 = new JTextField();
-		textField_24.setColumns(10);
-		textField_24.setBounds(721, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_24);
-		
+
 		textField_13 = new JTextField();
 		textField_13.setColumns(10);
 		textField_13.setBounds(338, 405, 95, 34);
 		frmValoracion.getContentPane().add(textField_13);
-		
-		textField_14 = new JTextField();
-		textField_14.setColumns(10);
-		textField_14.setBounds(338, 437, 95, 34);
-		frmValoracion.getContentPane().add(textField_14);
-		
-		JButton botonadd = new JButton("Añadir alimentos");
+
+		JButton botonadd = new JButton("Añadir");
 		botonadd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-		Alimento a1 = new Alimento(textField_16.getText(),Double.parseDouble(textField_9.getText()),Double.parseDouble(textField_11.getText()),
-				Double.parseDouble(textField_13.getText()),Double.parseDouble(textField_15.getText()),Double.parseDouble(textField_19.getText()),
-						Double.parseDouble(textField_21.getText()),Double.parseDouble(textField_23.getText()));
-		
-		Alimento a2 = new Alimento(textField_25.getText(),Double.parseDouble(textField_10.getText()),Double.parseDouble(textField_12.getText()),
-				Double.parseDouble(textField_14.getText()),Double.parseDouble(textField_18.getText()),Double.parseDouble(textField_20.getText()),
-						Double.parseDouble(textField_22.getText()),Double.parseDouble(textField_24.getText()));
-		
-		
-			listaAlimentos.add(a1);
-			listaAlimentos.add(a2);
-			
-			
+				Alimento a1 = new Alimento(textField_16.getText(), Double.parseDouble(textField_9.getText()),
+						Double.parseDouble(textField_11.getText()), Double.parseDouble(textField_13.getText()),
+						Double.parseDouble(textField_15.getText()), Double.parseDouble(textField_19.getText()),
+						Double.parseDouble(textField_21.getText()), Double.parseDouble(textField_23.getText()));
+
+				listaAlimentos.add(a1);
+				textField_16.setText("");
+				textField_9.setText("");
+				textField_11.setText("");
+				textField_13.setText("");
+				textField_15.setText("");
+				textField_19.setText("");
+				textField_21.setText("");
+				textField_23.setText("");
+				
+				textField_17.setText("");
+
 			}
 		});
-		botonadd.setBorder(new LineBorder(new Color(0, 100, 0), 3, true));
+		botonadd.setBorder(new LineBorder(new Color(0, 128, 0), 3, true));
 		botonadd.setBounds(53, 483, 139, 27);
 		frmValoracion.getContentPane().add(botonadd);
-		
-		JLabel lblSiPones = new JLabel("Si pones 0 no se añade.");
-		lblSiPones.setBounds(823, 48, 169, 17);
-		frmValoracion.getContentPane().add(lblSiPones);
+
+		lblGr = new JLabel("g");
+		lblGr.setBounds(926, 80, 60, 17);
+		frmValoracion.getContentPane().add(lblGr);
+
+		lblGr_1 = new JLabel("g");
+		lblGr_1.setBounds(926, 110, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_1);
+
+		lblGr_2 = new JLabel("g");
+		lblGr_2.setBounds(926, 145, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_2);
+
+		lblGr_3 = new JLabel("g");
+		lblGr_3.setBounds(926, 180, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_3);
+
+		lblGr_4 = new JLabel("g");
+		lblGr_4.setBounds(926, 210, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_4);
+
+		lblGr_5 = new JLabel("g");
+		lblGr_5.setBounds(926, 245, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_5);
+
+		lblGr_6 = new JLabel("g");
+		lblGr_6.setBounds(926, 275, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_6);
+
+		lblGr_7 = new JLabel("g");
+		lblGr_7.setBounds(926, 310, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_7);
+
+		lblGr_8 = new JLabel("g");
+		lblGr_8.setBounds(926, 340, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_8);
+
+		lblGr_9 = new JLabel("g");
+		lblGr_9.setBounds(926, 413, 60, 17);
+		frmValoracion.getContentPane().add(lblGr_9);
 	}
 }
